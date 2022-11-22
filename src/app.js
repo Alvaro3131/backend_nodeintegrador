61751353c9812b85537ec5b67648ffbb9947a316
Alvaro3131 <73666415+Alvaro3131@users.noreply.github.com>
2022-11-22 09:00:33 -0500

    asa

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-
+import authRoutes from "./routes/auth.routes";
 const app = express();
 var cors = require("cors");
 app.use(express.json());
@@ -12,5 +12,5 @@ app.get("/", (req, res) => {
     "<h1>Bienvenidos al Backend del Proyecto Integrador</h1><p>Desarrollado por el grupo N° 1</p>"
   );
 });
-
+app.use("/api/auth", authRoutes);
 export default app;
--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 
 export const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "ventas",
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "bcbbfa23540b29",
+  password: "8eedf331",
+  database: "heroku_b431d5eb0f8d695",
   port: 3306,
 });
