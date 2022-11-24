5faa965f53a34fe5f1c886c0e09c1b7dd595a20e
Alvaro3131 <73666415+Alvaro3131@users.noreply.github.com>
2022-11-24 00:33:06 -0500

    alvaro terminado casos

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,1 +1,82 @@
 import { pool } from "../database";
+const helpers = require("../libs/helpers");
+export const getSupervisores = async (req, res) => {
+  try {
+    pool.query(
+      "select * from usuario where id_rol=2;",
+
+      function (err, result) {
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar supervisores");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar error al listar supervisores");
+  }
+};
+export const getSupervisoresdni = async (req, res) => {
+  const id = parseInt(req.params.dni);
+  try {
+    pool.query(
+      "select * from usuario where id_rol=2 and num_doc=?;",
+      [id],
+      function (err, result) {
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar supervisores");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar error al listar supervisores");
+  }
+};
+
+export const createSupervisor = async (req, res) => {
+  const P_documento = req.body.documento;
+  const P_nombre = req.body.nombre;
+  const P_correo = req.body.correo;
+  const P_idtipodoc = req.body.tipodocumento;
+  const password = P_documento + P_documento.substr(0, 2);
+  const pass = await helpers.encryptPassword(password);
+  console.log(password);
+  try {
+    pool.query(
+      "INSERT INTO usuario (num_doc, nom_usuario, usuario, clave, correo, tipo_rol, estado_usuario, fecha_creacion, id_tipodoc, id_rol) VALUES(?, ?, ?, ?, ?, '', '1', now(), ?, 2);",
+      [P_documento, P_nombre, P_documento, pass, P_correo, P_idtipodoc],
+      function (err, result) {
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar supervisores");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar error al listar supervisores");
+  }
+};
+
+export const updateSupervisor = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const P_correo = req.body.correo;
+  try {
+    pool.query(
+      "UPDATE usuario set correo=? where id_usuario=?;",
+      [P_correo, id],
+      function (err, result) {
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar supervisores");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar error al listar supervisores");
+  }
+};
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import * as usuario from "../controllers/usuario.controller";
 const router = Router();
-
+router.get("/supervisor", usuario.getSupervisores);
+router.post("/supervisor", usuario.createSupervisor);
+router.get("/supervisor/:dni", usuario.getSupervisoresdni);
+router.put("/supervisor/:id", usuario.updateSupervisor);
 export default router;
