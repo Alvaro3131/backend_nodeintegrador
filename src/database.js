98bd0f7a6bf03d4a98020eec80a701b76a94df76
renbt30 <90786322+renbt30@users.noreply.github.com>
2025-01-14 21:11:35 -0500

    update database.js

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 
 export const pool = mysql.createPool({
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "bcbbfa23540b29",
-  password: "8eedf331",
-  database: "heroku_b431d5eb0f8d695",
-  port: 3306,
+  host: "monorail.proxy.rlwy.net",
+  user: "root",
+  password: "HEnoDMGmkJJOpejrKYQwtijWxKGPcChd",
+  database: "bd_ppp_nutricion",
+  port: 18946,
 });
